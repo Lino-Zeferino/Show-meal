c27241fcce9e74c76762ca308962e9d23d8cd4b6
Lino-Zeferino <linochimumaz@gmail.com>
2022-09-13 17:23:55 -0300

    subindo oprojeto

---
+++ lib/controller/controller.dart
@@ -0,0 +1,2 @@
+import 'package:aula10/data/my_data.dart';
+import 'package:aula10/module/SushiClass.dart';
---
+++ lib/data/my_data.dart
@@ -0,0 +1,15 @@
+import 'package:aula10/module/SushiClass.dart';
+
+// ignore: non_constant_identifier_names
+final MYTADA = [
+  Sushi("images/1.jpg", "Sutam witai", "1200"),
+  Sushi("images/4.jpg", "Riton SSui", "2000"),
+  Sushi("images/3.jpg", "carzh buit", "4500"),
+  Sushi("images/2.jpg", "Yurdt hutis", "1200"),
+  Sushi("images/Cachorro Quente.PNG", "Cachorro Quente", "200"),
+  Sushi("images/Batatas fritas.PNG", "Batatas fritas", "800"),
+  Sushi("images/Fango no forno com molho delicioso.PNG", "Fango no forno",
+      "5500"),
+  Sushi("images/Frango frito americano do KFC.jpg", "Frango do KFC", "3500"),
+  Sushi("images/Hamburguer.PNG", "Hamburguer", "1500"),
+];
